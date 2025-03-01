a8f4edcfd2d484ac859affb9bb03bea73cbf20a4
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-03-01 15:17:36 +0600

    random theme change functionality added

--- script/date.js
+++ script/date.js
@@ -7,4 +7,3 @@ const dateMonthYear = date.slice(3, 15);
 document.getElementById('day').innerText = day+" ,";
 // Set Date Month Year
 document.getElementById('date-month-year').innerText = dateMonthYear;
-
---
+++ script/theme-change.js
@@ -0,0 +1,12 @@
+// Theme Change Js
+document.getElementById('theme-btn').addEventListener('click', function(){
+
+    // Theme Button Rotate
+    let deg = Math.floor(Math.random() * 380)+90+"deg";
+    document.getElementById('theme-btn').style.transform = `rotate(${deg})`;
+
+    // Random Background Change
+    const hexCode = "#"+ Math.floor(Math.random() * 1000)+1;
+    console.log(hexCode);
+    document.getElementById('body').style.background = hexCode;
+});
